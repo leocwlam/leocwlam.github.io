51137de670184c6e58ea8aae4f6485d387137988
leo lam <leo.lam@uptake.com>
2020-08-26 08:13:54 -0400

    public

---
+++ precache-manifest.839a6b9011451d4d35e3c8027590ebb4.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8084d380f45151378aa495081a049bfe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cb9cf3ef0b9f2c327f5d",
+    "url": "/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "cb9cf3ef0b9f2c327f5d",
+    "url": "/static/js/2.13f5ec54.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.13f5ec54.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b290984275be243ae2c",
+    "url": "/static/js/main.85054244.chunk.js"
+  },
+  {
+    "revision": "e9a05e386ba5a5e1cf29",
+    "url": "/static/js/runtime-main.ce7ed91f.js"
+  },
+  {
+    "revision": "0765ef43e887595baacf05968d96a17d",
+    "url": "/static/media/chat-room-gate.0765ef43.jpg"
+  },
+  {
+    "revision": "65567a043ccbe3367ddf023a99a3b365",
+    "url": "/static/media/coffeeTime.65567a04.mp4"
+  },
+  {
+    "revision": "f8ef500c12c039f29dbef2ebf1e4b594",
+    "url": "/static/media/contact-image.f8ef500c.jpg"
+  },
+  {
+    "revision": "fbeb1605c936934dbe256f0625b362e4",
+    "url": "/static/media/github.fbeb1605.png"
+  },
+  {
+    "revision": "845427421019c80952e37eb17342becb",
+    "url": "/static/media/javascript.84542742.png"
+  },
+  {
+    "revision": "67ecec6e87d49f1deee474753b0271eb",
+    "url": "/static/media/not-found.67ecec6e.jpg"
+  },
+  {
+    "revision": "f3377053e91426a245f7c940f56c403a",
+    "url": "/static/media/open-source-house.f3377053.jpg"
+  },
+  {
+    "revision": "e7c202b26f8ed3e12b943ba90e624e1d",
+    "url": "/static/media/plenio-light.e7c202b2.jpg"
+  },
+  {
+    "revision": "0bd89efb9f7ab034fd7dc74f75115f98",
+    "url": "/static/media/profile-leo.0bd89efb.jpg"
+  }
+]);
